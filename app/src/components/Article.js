640af2c26016e272a99e2017fd52eda00302a87c
Oleg <o.selivancev@gmail.com>
2018-09-29 19:30:56 +0300

    react update

---
+++ app/src/components/Article.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Article(props){
+    const {article} =props;
+    console.log('----- Article Props', props);
+    const body = <section>{article.text}</section>;
+    return (
+        <div className ='hello' style={{color: 'green'}}>
+            <h2>{article.title}</h2>
+            {body}
+            <h3>creation date: {(new Date(article.date)).toDateString()}</h3>
+        </div>
+    )
+}
+
+export default Article;
--- app/src/index.js
+++ app/src/index.js
@@ -1,3 +1,8 @@
-/**
- * Created by admin on 28.09.2018.
- */
+import React from 'react';
+import {render} from 'react-dom';
+import App from './components/App'
+
+
+
+
+render(<App/>, document.getElementById('root'));
